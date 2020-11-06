ac238dcfe415b2db95760b4736f3f4651e70da24
Lucas Fernando <lucas98fernando@gmail.com>
2020-11-06 15:11:18 -0300

    Adjustments in gulpfile

--- layout/gulpfile.js
+++ layout/gulpfile.js
@@ -3,20 +3,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 // Task with gulp to convert Sass to CSS
-gulp.task('sass', function () {
+gulp.task('sass', gulp.series (function () {
     // Caminho dos arquivos que serão convertidos
     return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss']).pipe(sass()).pipe(gulp.dest('src/css'));
-});
-
-// Move the javascript files into our /src/js folder
-gulp.task('js', function () {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"));
-});
-
-// Static Server + watching scss/html files
-gulp.task('serve', gulp.series('sass', function () {
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.series('sass'));
 }));
 
-gulp.task('default', gulp.parallel('js', 'serve'));
+gulp.task('watch', gulp.series(function() {
+    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], gulp.parallel(['sass']));
+}));
+
+gulp.task('default', gulp.series(['sass', 'watch']));
